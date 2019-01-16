a9af5adb39a4c932573d5d03e78a1f5f29490d2d
josealique <33264489+josealique@users.noreply.github.com>
2019-01-16 18:20:23 +0100

    Add files via upload

--- Polynomial.java
+++ Polynomial.java
@@ -1,20 +1,21 @@
-
 public class Polynomial {
     private float[] coeficientes;
 
     // Constructor per defecte. Genera un polinomi zero
     public Polynomial() {
-
+        this.coeficientes = new float[]{0};
     }
 
     // Constructor a partir dels coeficients del polinomi en forma d'array
     public Polynomial(float[] cfs) {
-     this.coeficientes = cfs;
+        this.coeficientes = new float[cfs.length];
+        for (int i = 0; i < coeficientes.length; i++) {
+            coeficientes[i] = cfs[i];
+        }
     }
 
     // Constructor a partir d'un string
     public Polynomial(String s) {
-
     }
 
     // Suma el polinomi amb un altre. No modifica el polinomi actual (this). Genera un de nou
@@ -30,7 +31,7 @@ public class Polynomial {
     // Divideix el polinomi amb un altre. No modifica el polinomi actual (this). Genera un de nou
     // Torna el quocient i també el residu (ambdós polinomis)
     public Polynomial[] div(Polynomial p2) {
-       return null;
+        return null;
     }
 
     // Troba les arrels del polinomi, ordenades de menor a major
@@ -42,14 +43,22 @@ public class Polynomial {
     @Override
     public boolean equals(Object o) {
         Polynomial polynomial = (Polynomial) o;
-        if (this == polynomial) return true;
-        return false;
+        return this == polynomial;
     }
 
     // Torna la representació en forma de String del polinomi. Override d'un mètode de la classe Object
     @Override
     public String toString() {
-       if (coeficientes[0] == 1) return "x + " + coeficientes[1];
-       return "";
+        if (coeficientes[0] == 0) return "" + Math.round(coeficientes[0]);
+        if (coeficientes[0] == 1) return "x + " + Math.round(coeficientes[1]);
+        StringBuilder s = new StringBuilder(Math.round(coeficientes[0]) + "x^" + Math.round(coeficientes[1]));
+        for (int i = Math.round(coeficientes[0]) - 1; i >= 0; i--) {
+            if (coeficientes[i] == 0) continue;
+            else if (coeficientes[i] > 0) s.append(" + ").append(coeficientes[i]);
+            else if (coeficientes[i] < 0) s.append(" - ").append(-coeficientes[i]);
+            if (i == 1) s.append("x");
+            else if (i >  1) s.append("x^").append(i);
+        }
+        return s.toString();
     }
 }
