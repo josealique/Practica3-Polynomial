9f447d082a5ce58c3538492ed836b1408b19ba39
josealique <33264489+josealique@users.noreply.github.com>
2019-01-28 17:23:07 +0100

    Add files via upload
    Se arregla el metodo toString, falta el segundo constructor, las sumas y las multiplicaciones

--- Polynomial.java
+++ Polynomial.java
@@ -1,35 +1,32 @@
-import java.util.Arrays;
-
 public class Polynomial {
     private float[] coeficientes;
-    private String string;
+    private String poly;
 
     // Constructor per defecte. Genera un polinomi zero
     public Polynomial() {
         this.coeficientes = new float[]{0};
+        this.poly = "0";
     }
 
     // Constructor a partir dels coeficients del polinomi en forma d'array
     public Polynomial(float[] cfs) {
-        this.coeficientes = new float[cfs.length];
-        for (int i = 0; i < coeficientes.length; i++) {
-            coeficientes[i] = cfs[i];
-        }
+        this.coeficientes = cfs;
+        this.poly = "0";
     }
 
     // Constructor a partir d'un string
     public Polynomial(String s) {
         this.coeficientes = new float[]{0};
-        String[] componentes = s.split(" ");
-        for (int i = 0; i < componentes.length; i++) {
-            if (componentes[i].contains("x")){
-
-            }
-        }
+        this.poly = s;
     }
 
     // Suma el polinomi amb un altre. No modifica el polinomi actual (this). Genera un de nou
     public Polynomial add(Polynomial p) {
+        Polynomial polynomial = p;
+        Polynomial polynomial1 = this;
+        for (int i = 0; i < coeficientes.length; i++) {
+
+        }
         return null;
     }
 
@@ -59,16 +56,46 @@ public class Polynomial {
     // Torna la representació en forma de String del polinomi. Override d'un mètode de la classe Object
     @Override
     public String toString() {
-        StringBuilder s = new StringBuilder();
-        if (coeficientes[0] == 0) return "" + Math.round(coeficientes[0]);
-        if (coeficientes[0] == 1) return "x + " + Math.round(coeficientes[1]);
+        if (this.isZero()) return "0";
+        StringBuilder coso = new StringBuilder();
         for (int i = 0; i < coeficientes.length; i++) {
-            int number = Math.round(coeficientes[i]);
-            if (number == 0) continue;
-            if (number > 0) s.append(" + ");
-            s.append(number);
+            int coeff = Math.round(this.coeficientes[i]);
+            if (i == this.coeficientes.length - 1) {
+                coso.append(coeff);
+                continue;
+            }
+            int power = getPower(i);
+            if (power == 1) {
+                coso.append(coeff == 1 ? "x " : coeff + "x ");
+            } else {
+                coso.append(coeff == 1 ? "x^" + ++power + " " : coeff + "x^" + ++power + " ");
+                i += power - 2;
+            }
+            if (i + 1 <= this.coeficientes.length - 1) {
+                float next = this.coeficientes[i + 1];
+                coso.append(next > 0 ? "+ " : "- ");
+                this.coeficientes[i + 1] = next < 0 ? next * -1 : next;
+            }
         }
-        return s.toString();
+        return coso.toString();
     }
 
+    private boolean isZero() {
+        boolean empty = true;
+        for (float f : this.coeficientes) {
+            if (f != 0.0) {
+                empty = false;
+            }
+        }
+        if (!this.poly.equals("0")) empty = false;
+        return empty;
+    }
+
+    private int getPower(int idx) {
+        int counter = 0;
+        do {
+            counter++;
+        } while (this.coeficientes[++idx] == 0);
+        return counter;
+    }
 }
